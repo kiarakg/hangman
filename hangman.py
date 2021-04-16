66001e945c694ccc220d5d93b5750a529e16845e
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-16 03:15:15 -0400

    get_valid_word function

--- hangman.py
+++ hangman.py
@@ -1,4 +1,10 @@
 import random
 from words import words
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words)     # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
