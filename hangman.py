23fbb6ee493312cda7782bee8e6294cb3a2a225f
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-16 03:22:35 -0400

    what the user has guessed

--- hangman.py
+++ hangman.py
@@ -1,10 +1,19 @@
 import random
 from words import words
+import string
 
 def get_valid_word(words):
     word = random.choice(words)     # randomly chooses something from the list
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word
+    return word.upper()
+
+# keeping track of letters we've guessed, correctly guessed, and what is and isn't a valid letter
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)     # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()     # what the user has guessed
+
 
