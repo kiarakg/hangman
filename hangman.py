c97a20fbf7453d17727128c99c3632c3564badf7
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-16 12:09:25 -0400

    user input

--- hangman.py
+++ hangman.py
@@ -7,7 +7,7 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word.upper()
+    return word
 
 # keeping track of letters we've guessed, correctly guessed, and what is and isn't a valid letter
 def hangman():
@@ -17,13 +17,31 @@ def hangman():
     used_letters = set()     # what the user has guessed
 
     # getting the user input
-    user_letter = input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (i.e. W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+    # gets here when len(word_letters) == 0
 
 
-user_input = input("Type something: ")
+
+user_input = input('Type something: ')
 print(user_input)
